6e81ea61f590197cf03b99aa452686dc62575789
gustavohideki@outlook.com <40206355+HidekiGus@users.noreply.github.com>
2022-05-02 06:53:06 -0300

    Projeto finalizado

--- scripts.js
+++ scripts.js
@@ -5,12 +5,17 @@ let nome = "";
 let modeloEscolhido;
 let golaEscolhida;
 let tecidoEscolhido;
+let promessaRequisitaRecentes;
+let dadosGet;
+
+
+
+perguntaNome();
+extraiAPI();
 
 function perguntaNome() {
     nome = prompt("Qual é o seu nome?");
 }
-
-perguntaNome();
 
 function selecionarModelo(elemento) {
     let itemSelecionado = document.querySelector(".modelos .selecionado");
@@ -84,10 +89,85 @@ function confirmarPedido() {
     promessa.catch(alertaErro);
 }
 
+function pegaIndex(elemento) {
+
+}
+
+function confirmarPedidoUltimosPedidos(elemento) {
+
+    let indice = elemento.classList[1];
+    let extrairDados = dadosGet[indice];
+    let modelo = extrairDados.model;
+    let gola = extrairDados.neck;
+    let tecido = extrairDados.material;
+
+
+    if (window.confirm(`Você quer pedir essa peça?
+    Características: 
+        -Modelo: ${modelo}
+        -Gola: ${gola}
+        -Tecido: ${tecido}`)) {
+
+        let objeto = {
+            model: modelo,
+            neck: gola,
+            material: tecido,
+            image: extrairDados.image,
+            owner: nome,
+            author: nome
+        };
+
+        let promessa = axios.post(API, objeto);
+        promessa.then(alertaConfirmacao);
+        promessa.catch(alertaErro);
+    }
+}
+
+function loopTeste(valor) {
+    for (i=0; i<dadosGet.length; i++) {
+        if (dadosGet[i].id === valor) {
+            return valor;
+        }
+    }
+}
+
 function alertaConfirmacao() {
+    extraiAPI();
     alert("Encomenda confirmada!");
 }
 
 function alertaErro() {
     alert("Opa, não conseguimos processar sua encomenda!");
 }
+
+function extraiAPI() {
+    promessaRequisitaRecentes = axios.get(API);
+    promessaRequisitaRecentes.then(salvarDados);
+}
+
+function salvarDados(dados) {
+    dadosGet = dados.data;
+    mostrarRecentes();
+}
+
+function mostrarRecentes() {
+    console.log(dadosGet);
+    let opcoes = document.querySelector(".barra-bottom .opcoes");
+    opcoes.innerHTML = "";
+    let tamanho = Number(dadosGet.length);
+
+    for (let i = 0; i<tamanho; i++) {
+        let nome = dadosGet[i].owner;
+        console.log(nome);
+        let imagem = dadosGet[i].image;
+        opcoes.innerHTML += `
+        <div class="opcao ${i}" onclick="confirmarPedidoUltimosPedidos(this)">
+            <img src=${imagem} />
+            <div class="criador">
+                Criador: <span>${nome}</span>
+            </div>
+        </div>        
+        `
+    }
+}
+
